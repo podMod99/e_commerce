4c9f432dc8ab741383417ab90d6f241ee8d3fa7f
podMod99 <Felix.butcher@gmx.com>
2020-12-27 17:01:46 +0100

    Added Navbar component and styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Products from './components/products/Products';
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
   );
