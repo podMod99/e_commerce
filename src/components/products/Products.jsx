b8a011ef01dd20c6e81bdbeef94f3c6549ce967c
podMod99 <Felix.butcher@gmx.com>
2020-12-28 10:30:12 +0100

    Added test products in commerce JS. linked them with website

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,26 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './product/Product';
 import useStyles from './styles';
 
-const products = [
-  {
-    id: 1,
-    name: 'shoes',
-    description: 'Running Shoes.',
-    price: '£5',
-    image:
-      'https://marks.scene7.com/is/image/marksp/290913_BQ3204-002_PROD_2_BLACK?wid=1488&hei=1800&fit=fit,1&bgColor=0,0,0,0&op_sharpen=1&resMode=sharp2&fmt=jpg&qlt=85,0',
-  },
-  {
-    id: 2,
-    name: 'Macbook',
-    description: 'Apple macbook.',
-    price: '£10',
-    image:
-      'https://www.humac.dk/sites/default/files/styles/large/public/product-images/2020-05/WWEN_MacBook_Pro_13in_2ports_space_gray_PDP_US_1_2.jpg?itok=D2WSO-I-',
-  },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
   const classes = useStyles();
   return (
     <main className={classes.content}>
