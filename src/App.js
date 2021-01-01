309da22eb07104f1cf3e892db2aa3d6b1a86661e
podMod99 <Felix.butcher@gmx.com>
2021-01-01 17:09:54 +0100

    Fixed infinite re render bug. Address form finnished.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
 import Products from './components/products/Products';
 import Navbar from './components/navbar/Navbar';
+import Cart from './components/cart/Cart';
+import Checkout from './components/checkoutForm/checkout/Checkout';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  console.log('app render');
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -12,16 +17,61 @@ const App = () => {
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const { cart } = await commerce.cart.add(productId, quantity);
+
+    setCart(cart);
+  };
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+
+    setCart(cart);
+  };
+
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+
+    setCart(cart);
+  };
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+
+    setCart(cart);
+  };
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
 
-  console.log(products);
   return (
-    <div>
-      <Navbar />
-      <Products products={products}/>
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path='/'>
+            <Products products={products} onAddToCart={handleAddToCart} />
+          </Route>
+          <Route exact path='/cart'>
+            <Cart
+              cart={cart}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+              handleEmptyCart={handleEmptyCart}
+            />
+          </Route>
+          <Route exact path='/checkout'>
+            <Checkout cart={cart} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
